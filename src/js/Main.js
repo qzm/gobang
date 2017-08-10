7a67d2b116aa10799cdfcae7902fa3f514cb2cc1
qiuzhiming <qiuzhiming@hdfax.com>
2017-08-10 15:55:27 +0800

    增加Debug

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,12 +1,14 @@
 import { run } from './Util';
-import { PieceContraller } from './Contrallers';
-import { PieceView } from './Views';
-import { PieceModel } from './Models';
-
-let piece = pool.create();
-piece = new PieceContraller(new PieceView(new PieceModel({ name: 'Piece' })));
+import { Contraller, PieceContraller } from './Contrallers';
+import { View, PieceView } from './Views';
+import { Model, PieceModel } from './Models';
+// hello
+const hello = new Contraller(new View(new Model({ name: 'Welcome to this game!' })));
+console.info(hello.getName());
 
 //  动画循环
-run(() => {
-  console.log(piece);
-})
+function animation() {
+  // some step
+  run(animation);
+}
+run(animation);
